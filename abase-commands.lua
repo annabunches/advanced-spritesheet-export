ac746e84c33339ecd631e56ed58c09b1ff00ad54
annabunches <annabunches@gmail.com>
2024-07-29 21:25:14 -0400

    Basic functionality in place.

---
+++ abase-commands.lua
@@ -0,0 +1,45 @@
+-- New commands to be executed via Aseprite menus / keyboard shortcuts
+local sprt = require "abase-sprite"
+
+local function ExportSpritesheetAdvanced()
+    if not app.sprite then
+        return app.alert "Must have a sprite open to export."
+    end
+
+    local spr = Sprite(app.sprite)
+
+    sprt.deleteLayers(spr, spr.layers)
+    sprt.flattenLayers(spr.layers)
+    sprt.revealLayers(spr.layers)
+
+    app.command.ExportSpriteSheet {
+        splitLayers = true
+    }
+
+    spr:close()
+end
+
+local function ToggleIgnore()
+    local layer = app.layer
+    if (layer.properties(extKey).ignored) then
+        layer.properties(extKey).ignored = false
+    else
+        layer.properties(extKey).ignored = true
+    end
+end
+
+local function ToggleExportAsSprite()
+    local layer = app.layer
+    if (layer.properties(extKey).exportedAsSprite) then
+        layer.properties(extKey).exportedAsSprite = false
+    else
+        layer.properties(extKey).exportedAsSprite = true
+    end
+end
+
+local export = {
+    ExportSpritesheetAdvanced = ExportSpritesheetAdvanced,
+    ToggleIgnore = ToggleIgnore,
+    ToggleExportAsSprite = ToggleExportAsSprite
+}
+return export
